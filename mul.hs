d7c085bb4425d28b034e26a08de4697af39197ad
Ranieri Althoff <ranisalt@gmail.com>
2015-03-27 03:37:24 -0300

    Add fast multiplication problem (MUL)

---
+++ mul.hs
@@ -0,0 +1,15 @@
+import System.Exit
+
+mul [x, y] = xInt * yInt
+	where xInt = read x :: Integer
+	      yInt = read y :: Integer
+
+mainLoop 0 = exitWith ExitSuccess
+mainLoop x = do
+	values <- getLine
+	print $ mul (take 2 $ words values)
+	mainLoop (x - 1)
+
+main = do
+	loops <- getLine
+	mainLoop (read loops :: Integer)
